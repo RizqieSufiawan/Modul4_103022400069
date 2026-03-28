439c7f6c688f1cd6d9b0baa06730da0de34a59a0
RizqieSufiawan <iamrizqie@gmail.com>
2026-03-28 14:06:24 +0700

    second commit

---
+++ KodePaket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modul4_103022400069
+{
+    internal class KodePaket
+    {
+        public string GetKodePaket(string namaPaket)
+        {
+            string[,] tableKodePaket = {
+        { "Basic", "P201" },
+        { "Standard", "P202" },
+        { "Premium", "P203" },
+        { "Unlimited", "P204" },
+        { "Gaming", "P205" },
+        { "Streaming", "P206" },
+        { "Family", "P207" },
+        { "Business", "P208" },
+        { "Student", "P209" },
+        { "Traveler", "P210" },
+        };
+            for (int i = 0; i < tableKodePaket.GetLength(0); i++)
+            {
+                if (tableKodePaket[i, 0].Equals(namaPaket, StringComparison.OrdinalIgnoreCase))
+                {
+                    return tableKodePaket[i, 1];
+                }
+            }
+            return "Paket Tidak Ditemukan";
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            KodePaket kodePaket = new KodePaket();
+
+            Console.WriteLine("Basic Package: " + kodePaket.GetKodePaket("Basic"));
+            Console.WriteLine("Standard Package: " + kodePaket.GetKodePaket("Standard"));
+            Console.WriteLine("Premium Package: " + kodePaket.GetKodePaket("Premium")); 
+            Console.WriteLine("Unlimited Package: " + kodePaket.GetKodePaket("Unlimited")); 
+            Console.WriteLine("Gaming Package: " + kodePaket.GetKodePaket("Gaming"));
+            Console.WriteLine("Streaming Package: " + kodePaket.GetKodePaket("Streaming"));
+            Console.WriteLine("Family Package: " + kodePaket.GetKodePaket("Family"));
+            Console.WriteLine("Business Package: " + kodePaket.GetKodePaket("Business"));
+            Console.WriteLine("Student Package: " + kodePaket.GetKodePaket("Student"));
+            Console.WriteLine("Traveler Package: " + kodePaket.GetKodePaket("Traveler"));
         }
     }
 }
